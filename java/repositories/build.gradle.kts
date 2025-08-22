8f0c97dc4dae0735fd2ec9f6b6fb692a440cdbae
fResult <styxmaz@gmail.com>
2025-08-23 00:08:17 +0700

    Upgrade Spring Boot from 3.5.4 to 3.5.5.

--- java/repositories/build.gradle.kts
+++ java/repositories/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   java
-  id("org.springframework.boot") version "3.5.4"
+  id("org.springframework.boot") version "3.5.5"
   id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -20,10 +20,13 @@ repositories {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+
   runtimeOnly("com.h2database:h2")
   runtimeOnly("io.r2dbc:r2dbc-h2")
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("io.projectreactor:reactor-test")
+
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- kotlin/services/build.gradle.kts
+++ kotlin/services/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   kotlin("jvm") version "2.2.0"
   kotlin("plugin.spring") version "2.2.0"
-  id("org.springframework.boot") version "3.5.4"
+  id("org.springframework.boot") version "3.5.5"
   id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -24,10 +24,12 @@ dependencies {
   implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
